b3265a80b2c1fd4418c298f504903c5cbfb78b75
João Xavier <fastjonh@gmail.com>
2021-07-30 14:36:13 -0300

    Implementei o corpo da classe calculadora

---
+++ Calculadora/Calculadora/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calculadora
+    {
+        private delegate Calculo Operacao(double num1, double num2);
+        List<Calculo> historico = new List<Calculo>();
+        public Calculadora()
+        {
+        }
+
+        private Calculo Calcular(string num1, string num2, int codOp)
+        {
+            return null;
+        }
+        private Operacao GetOperacao(TipoOperacao op)
+        {
+            throw new NotImplementedException();
+        }
+        private Calculo Calcular(double num1, double num2, Operacao op)
+        {
+            return op(num1, num2);
+        }
+
+        public enum TipoOperacao
+        {
+            adicao, subtracao, multiplicacao, divisao
+        }
+    }
+}
